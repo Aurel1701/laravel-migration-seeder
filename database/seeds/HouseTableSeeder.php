1f25f73ef195d5382b04677c5e5e150fe720316b
Aurel1701 <gantzfan96@gmail.com>
2022-06-15 10:15:40 +0200

    add table and foreach

--- database/seeds/HouseTableSeeder.php
+++ database/seeds/HouseTableSeeder.php
@@ -11,22 +11,67 @@ class HouseTableSeeder extends Seeder
      */
     public function run()
     {
+        /* $houses = config('db.houses'); */
         $houses = [
             [
-              'address' => '',
-              'city' => '',
-              'zip_code' => '',
-              'country' => '',
-              'county' => '',
-              'square_meters' => '',
-              'rooms' => '',
-              'type' => '',
-              'for_sale' => '',
-              'to_let' => '',
-              'is_available' => '',
-              'price' => ''
-            ], 
-            # Fill the data, then copy paste to add more entries.
+                'address' => '5th avenue',
+                'city' => 'New York City',
+                'zip_code' => 'NYC-001',
+                'country' => 'USA',
+                'county' => 'New York',
+                'square_meters' => 200,
+                'rooms' => 5,
+                'type' => 'Villa',
+                'for_sale' => true,
+                'to_let' => false,
+                'is_available' => true,
+                'price' => 400000
+            ],      [
+                'address' => 'central park boulevard',
+                'city' => 'New York City',
+                'zip_code' => 'NYC-005',
+                'country' => 'USA',
+                'county' => 'New York',
+                'square_meters' => 300,
+                'rooms' => 7,
+                'type' => 'Palace',
+                'for_sale' => true,
+                'to_let' => false,
+                'is_available' => true,
+                'price' => 600000
+            ],
+            [
+                'address' => 'Brookleen road',
+                'city' => 'Brookleen',
+                'zip_code' => 'BRK-007',
+                'country' => 'USA',
+                'county' => 'New York',
+                'square_meters' => 100,
+                'rooms' => 3,
+                'type' => 'Apartment',
+                'for_sale' => false,
+                'to_let' => true,
+                'is_available' => true,
+                'price' => 1000
+            ],
+    
         ];
+
+        foreach ($houses as $house) {
+            $_house = new House(); 
+            $_house->address = $house['address'];
+            $_house->city = $house['city'];
+            $_house->zip_code = $house['zip_code'];
+            $_house->country = $house['country'];
+            $_house->county = $house['county'];
+            $_house->square_meters = $house['square_meters'];
+            $_house->rooms = $house['rooms'];
+            $_house->type = $house['type'];
+            $_house->for_sale = $house['for_sale'];
+            $_house->to_let = $house['to_let'];
+            $_house->is_available = $house['is_available'];
+            $_house->price = $house['price'];
+            $_house->save(); 
+        }
     }
 }
--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -16,8 +16,11 @@
     </head>
     <body>
     <header class="">
-        
-        
+        <nav class="nav justify-content-center">
+          <a class="nav-link active" href="#">Active link</a>
+          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link disabled" href="#">Holidays</a>
+        </nav>
     </header>
 
     <main>
